c457a9c0e22d1b2920708bfe6f12b7f6f7be7f17
VNxVTTr <22521573@gm.uit.edu.vn>
2024-11-13 17:26:34 +0700

    local_end

--- test.py
+++ test.py
@@ -6,10 +6,9 @@ print('write second')
 
 print('write on local 2')
 
-<<<<<<< HEAD
 print('write on loca l 3')
-=======
 print('write on web')
->>>>>>> db0e87e0606c944e61a2fbb7e68df55b6e8c00ea
 
 print('end')
+
+print('local_end')
